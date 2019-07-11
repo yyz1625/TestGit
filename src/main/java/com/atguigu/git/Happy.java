5d76030a07d1b67aab52d3cb4675f520ed161fad
tom_glb <goodMorning_pro@atguigu.com>
2019-07-11 10:58:17 +0800

    Merge branch 'master' of https://github.com/yyz1625/TestGit.git
    Conflicts:
    	src/main/java/com/atguigu/git/Happy.java

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Right....");
-		System.out.println("Left....");
+		System.out.println("Left....yyyyy");
 	
 	}
 }
